6baf7145442bfcf725aa621ea6f5a7c800229d0e
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-16 11:32:49 +0200

    make class and a window with differnt color

--- src/pytris/__init__.py
+++ src/pytris/__init__.py
@@ -1,7 +1,26 @@
 import arcade
 
 
+class Game(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.background_color = arcade.color.AERO_BLUE
+
+    def on_draw(self):
+        self.clear()
+
+    def on_update(self, dt):
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        pass
+
+
 def main() -> None:
     print("Hello from pytris!")
-    arcade.open_window(400, 400, "Hello World")
+    window = arcade.Window(width=400, height=300, title="Pytris")
+
+    game = Game()
+    window.show_view(game)
+
     arcade.run()
