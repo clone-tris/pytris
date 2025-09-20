df18005b9b4a36a2a1c6b534de1394e2c8497aa4
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-20 21:43:47 +0200

    mockup of how it would look like in python

--- src/pytris/__init__.py
+++ src/pytris/__init__.py
@@ -1,56 +1,77 @@
+from typing import override
 import pygame
 
 
+class Screen:
+    def update(self, dt: float) -> str | None:  # pyright: ignore[reportUnusedParameter]
+        pass
+
+    def draw(self, surface: pygame.Surface):  # pyright: ignore[reportUnusedParameter]
+        pass
+
+
+class GameScreen(Screen):
+    player_pos: pygame.Vector2 = pygame.Vector2(0, 0)
+
+    @override
+    def update(self, dt: float) -> str | None:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.player_pos.y -= 300 * dt
+        if keys[pygame.K_s]:
+            self.player_pos.y += 300 * dt
+        if keys[pygame.K_a]:
+            self.player_pos.x -= 300 * dt
+        if keys[pygame.K_d]:
+            self.player_pos.x += 300 * dt
+        if keys[pygame.K_q]:
+            return "QUIT_PLEASE"
+
+    @override
+    def draw(self, surface: pygame.Surface):
+        surface.fill("#660e7a")
+        player_pos = pygame.Vector2(surface.get_width() / 2, surface.get_height() / 2)
+        pygame.draw.circle(surface, "#99ccff", player_pos, 40)
+
+
 class Game:
+    running: bool = True
+    screen: Screen
+    surface: pygame.Surface
+    clock: pygame.Clock
+    dt: float
+
     def __init__(self) -> None:
-        print("init")
+        pygame.init()
+        self.surface = pygame.display.set_mode(size=(1280, 720))
+        self.clock = pygame.time.Clock()
+        self.screen = GameScreen()
+        self.dt = 0
+        self.running = True
 
     def draw(self):
-        print("draw")
+        self.screen.draw(self.surface)
 
     def update(self):
-        print("update")
+        screen_event = self.screen.update(self.dt)
+        if screen_event == "QUIT_PLEASE":
+            self.running = False
 
     def loop(self):
-        print("loop")
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.update()
+            self.draw()
+
+            pygame.display.flip()
+
+            self.dt = self.clock.tick(60) / 1000
+
+        pygame.quit()
 
 
 def main() -> None:
-    pygame.init()
-    screen = pygame.display.set_mode(size=(1280, 720))
-    clock = pygame.time.Clock()
-    running = True
-    dt = 0
-
-    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill("#660e7a")
-
-        pygame.draw.circle(screen, "#99ccff", player_pos, 40)
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
-        if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
-        if keys[pygame.K_a]:
-            player_pos.x -= 300 * dt
-        if keys[pygame.K_d]:
-            player_pos.x += 300 * dt
-        if keys[pygame.K_q]:
-            running = False
-
-        # flip() the display to put your work on screen
-        pygame.display.flip()
-
-        # limits FPS to 60
-        # dt is delta time in seconds since last frame, used for framerate-
-        # independent physics.
-        dt = clock.tick(60) / 1000
-
-    pygame.quit()
+    game = Game()
+    game.loop()
