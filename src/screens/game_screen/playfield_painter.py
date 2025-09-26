947842ff942307a5e1e41b8250207fd4f6e18684
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-26 19:42:52 +0200

    draw background new way

--- src/screens/game_screen/playfield_painter.py
+++ src/screens/game_screen/playfield_painter.py
@@ -1,13 +1,22 @@
+import pygame
 from pygame import Rect
 
+import colors
 from config import CANVAS_HEIGHT, SIDEBAR_WIDTH, WAR_ZONE_WIDTH
 from engine.painter import Painter
 from screens.game_screen.components.shape import Shape
 
+PLAYFIELD_RECT = Rect(SIDEBAR_WIDTH, 0, WAR_ZONE_WIDTH, CANVAS_HEIGHT)
+
 
 class GamePainter(Painter):
     def draw_playfield(self, player: Shape, opponent: Shape):
-        self.draw_guide(rect=Rect(SIDEBAR_WIDTH, 0, WAR_ZONE_WIDTH, CANVAS_HEIGHT))
+        pygame.draw.rect(
+            surface=self.surface,
+            color=colors.Ui.BACKGROUND.value,
+            rect=PLAYFIELD_RECT,
+        )
+        self.draw_guide(rect=PLAYFIELD_RECT)
         self.draw_shape(shape=opponent, ref=(SIDEBAR_WIDTH, 0))
         self.draw_shape(shape=player, ref=(SIDEBAR_WIDTH, 0))
 
