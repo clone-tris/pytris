4c56cf8362bcef385ea76e12ec9c56af69e91bff
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-26 16:46:45 +0200

    draw all borders

--- src/screens/game_screen/game_painter.py
+++ src/screens/game_screen/game_painter.py
@@ -1,16 +1,73 @@
 import pygame
+import pygame.gfxdraw
+
+import colors
 import config
 from engine.painter import Painter
 from screens.game_screen.components.shape import Shape
+
+WIDTH = config.SQUARE_WIDTH
+BORDER = config.SQUARE_BORDER_WIDTH
+INNER = WIDTH - BORDER * 2
 
 
 class GamePainter(Painter):
     def draw_shape(self, shape: Shape):
         for square in shape.squares:
-            x = (shape.column + square.column) * config.SQUARE_WIDTH
-            y = (shape.row + square.row) * config.SQUARE_WIDTH
+            x = (shape.column + square.column) * WIDTH
+            y = (shape.row + square.row) * WIDTH
+
+            # background
             pygame.draw.rect(
                 self.surface,
                 square.color,
-                [x, y, config.SQUARE_WIDTH, config.SQUARE_WIDTH],
+                [x, y, WIDTH, WIDTH],
+            )
+
+            # Left Border
+            pygame.gfxdraw.filled_polygon(
+                self.surface,
+                [
+                    (x, y),
+                    (x + BORDER, y + BORDER),
+                    (x + BORDER, y + WIDTH - BORDER),
+                    (x, y + WIDTH),
+                ],
+                colors.Square.BORDER_SIDE.value,
+            )
+
+            # Right Border
+            pygame.gfxdraw.filled_polygon(
+                self.surface,
+                [
+                    (x + WIDTH, y),
+                    (x + WIDTH - BORDER, y + BORDER),
+                    (x + WIDTH - BORDER, y + WIDTH - BORDER),
+                    (x + WIDTH, y + WIDTH),
+                ],
+                colors.Square.BORDER_SIDE.value,
+            )
+
+            # Top Border
+            pygame.gfxdraw.filled_polygon(
+                self.surface,
+                [
+                    (x, y),
+                    (x + BORDER, y + BORDER),
+                    (x + WIDTH - BORDER, y + BORDER),
+                    (x + WIDTH, y),
+                ],
+                colors.Square.BORDER_TOP.value,
+            )
+
+            # Bottom Border
+            pygame.gfxdraw.filled_polygon(
+                self.surface,
+                [
+                    (x, y + WIDTH),
+                    (x + BORDER, y + WIDTH - BORDER),
+                    (x + WIDTH - BORDER, y + WIDTH - BORDER),
+                    (x + WIDTH, y + WIDTH),
+                ],
+                colors.Square.BORDER_BOTTOM.value,
             )
