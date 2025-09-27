0af77cf6b536b8af72ab17b03e47b293bf6da78d
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-27 20:13:27 +0200

    move player here and there

--- src/screens/game_screen/__init__.py
+++ src/screens/game_screen/__init__.py
@@ -28,7 +28,7 @@ class GameScreen(Screen):
         self.score = Score()
         self.next_player = random_tetromino()
         self.opponent = get_tetromino(tetromino.Name.I)
-        self.opponent.row = 4
+        self.opponent.row = 7
         self.opponent.column = 4
         self.should_quit = False
         self.spawn_player()
@@ -52,11 +52,14 @@ class GameScreen(Screen):
         match key:
             case pygame.K_q:
                 self.should_quit = True
-            case pygame.K_r:
+            case pygame.K_UP | pygame.K_w | pygame.K_SPACE:
                 self.rotate_player()
-            case pygame.K_s:
-                self.spawn_player()
-                self.player.row = 2
+            case pygame.K_a | pygame.K_LEFT:
+                self.move_player_left()
+            case pygame.K_d | pygame.K_RIGHT:
+                self.move_player_right()
+            case pygame.K_s | pygame.K_DOWN:
+                self.move_player_down()
             case _:
                 pass
 
@@ -82,8 +85,33 @@ class GameScreen(Screen):
         if self.paused:
             return
 
-        foreshadow = copy.deepcopy(self.player)
+        foreshadow = self.player.copy()
         foreshadow.rotate()
 
         if not foreshadow.collides_with(self.opponent) and foreshadow.within_bounds():
             self.player = foreshadow
+
+    def move_player_left(self):
+        self.move_player(0, -1)
+
+    def move_player_right(self):
+        self.move_player(0, 1)
+
+    def move_player_down(self):
+        self.move_player(1, 0)
+
+    def move_player(self, row: int, column: int):
+        if self.paused:
+            return
+
+        foreshadow = self.player.copy()
+        foreshadow.translate(row=row, column=column)
+
+        able_to_move = (
+            not foreshadow.collides_with(self.opponent) and foreshadow.within_bounds()
+        )
+
+        if able_to_move:
+            self.player = foreshadow
+
+        return able_to_move
